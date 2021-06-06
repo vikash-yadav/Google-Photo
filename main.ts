4c0722a30bb29e59e3a3c6613231a8b03b4f16e1
vikash-yadav <vikash.yadav9911@gmail.com>
2021-06-06 14:39:14 +0530

    First Commit

--- main.ts
+++ main.ts
@@ -1,12 +1,11 @@
 var AWS = require("aws-sdk");
-AWS.config.update({accesKeyId:'AKIA3PL3WK5VXEJP44HU', secretAccessKey: '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 ', region:'Asia Pacific (Mumbai) ap-south-1'});
+AWS.config.update({accesKeyId:'AKIA3PL3WK5VXEJP44HU', secretAccessKey: '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 ', region:'us-east-1'});
 var s3 = new AWS.S3();
 
 var params = {
   Bucket: "profile-images.test.com",
   Delimiter: "/",
-  prefix: " ",
-};
+ };
 
 s3.listObjects(params, function (err, data) {
   if (err) throw err;
